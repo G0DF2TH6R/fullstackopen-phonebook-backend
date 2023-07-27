e661e99037e344a18a9dbed91d29be71e8abbe81
Chris Matsiselts <85128734+G0DF2TH6R@users.noreply.github.com>
2023-07-27 19:55:42 +0300

    update frontend build

--- index.js
+++ index.js
@@ -75,14 +75,13 @@ app.get('/info', async (request, response) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+    const { name, number } = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(req.params.id, person)
+    Person.findByIdAndUpdate(
+        req.params.id, 
+        {name, number},
+        { new: true, runValidators: true, context: 'query'}
+        )
     .then(updatedPerson => res.json(updatedPerson))
     .catch(error => next(error))
 
